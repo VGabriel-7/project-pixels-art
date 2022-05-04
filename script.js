6cd866ad27e82aa3e961e80065615bbd48ddc087
Vinicius Gabriel <viniciusgsa99@gmail.com>
2022-05-03 22:24:29 -0300

    Requisitos 11 e 12 completos, adiciona um limite mínimo e máximo para o board ser gerado caso não insira nada no input um alert é mostrado na tela, adiciona cores aleatórias cada vez que a página é regarregada. VQV!

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const pixelBoard = document.getElementById('pixel-board');
 const colorPalette = document.querySelectorAll('#color-palette div');
+const firstColor = document.querySelector('#black');
 const buttonClear = document.getElementById('clear-board');
 const btnVqv = document.getElementById('generate-board');
+const input = document.getElementById('board-size');
+
+for (let index = 0; index < colorPalette.length; index += 1) {
+  const red = Math.floor(Math.random() * 256); // referência da explicação do código utilizado https://www.w3schools.com/js/js_random.asp
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  const randonColor = `rgb(${red}, ${green}, ${blue})`;
+  const element = colorPalette[index];
+  element.style.backgroundColor = randonColor;
+  firstColor.style.backgroundColor = 'black';
+}
 
 function setTheColor() {
   const selected = document.querySelector('.selected');
   // eslint-disable-next-line no-restricted-globals
-  event.target.id = selected.id;
+  event.target.style.backgroundColor = selected.style.backgroundColor;
 }
 
 function initialBoard() {
@@ -20,13 +32,22 @@ function initialBoard() {
 
 initialBoard();
 
+function limitBoard() {
+  if (input.value < 5) {
+    input.value = 5;
+  } else if (input.value > 50) {
+    input.value = 50;
+  }
+}
+
 btnVqv.addEventListener('click', () => {
+  console.log(input.value);
   pixelBoard.innerText = '';
-  const input = document.getElementById('board-size');
-  if (input.value < 1) {
+  if (input.value == '') {
     initialBoard();
     return alert('Board inválido!');
   }
+  limitBoard();
   pixelBoard.style.width = `${42 * input.value}px`;
   for (let index = 0; index < input.value * input.value; index += 1) {
     const pixels = document.createElement('div');
@@ -55,7 +76,7 @@ for (let index = 0; index < colorPalette.length; index += 1) {
 function clearBoard() {
   for (let index = 0; index < pixelBoard.childNodes.length; index += 1) {
     const element = pixelBoard.childNodes[index];
-    element.id = 'white';
+    element.style.backgroundColor = 'white';
   }
 }
 
